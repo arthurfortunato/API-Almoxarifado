c12a3caa5a0bdc01a1ddfc284bd6e623a73311aa
arthurfortunato <luizarthur.fortunato@gmail.com>
2022-03-18 11:28:39 -0300

    Iniciando as rotas da aplicação

---
+++ src/routes/product.routes.ts
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+import { CreateProductController } from '../controller/CreateProductController';
+
+export const productRoutes = Router();
+const productController = new CreateProductController();
+
+productRoutes.post('/newproduct', productController.handle);
+
