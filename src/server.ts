f7f8c07fbe9a428f38ed925841c9b14caabafde4
arthurfortunato <luizarthur.fortunato@gmail.com>
2022-03-18 12:38:50 -0300

    Adicionando o cors na aplicação

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,18 @@
 import 'express-async-errors'
+import cors from 'cors';
+
 import express from "express";
 import "reflect-metadata";
 import { createConnection } from "typeorm";
 
 import { routes } from './routes/index';
-import { globalErrors } from "./middlewares/globalErrors";
+import { globalErrors } from "./middleware/globalErrors";
 
 createConnection().then(() => {
   const app = express();
   const PORT = 4000;
 
+  app.use(cors());
   app.use(express.json());
   app.use(routes);
   app.use(globalErrors)
