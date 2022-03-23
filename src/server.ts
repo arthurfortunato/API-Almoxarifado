24ca168fa3afdbbaefe5708f77f441e1f1b6a82d
arthurfortunato <luizarthur.fortunato@gmail.com>
2022-03-23 10:19:49 -0300

    Atualizando para deploy

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,7 @@ import { globalErrors } from "./middleware/globalErrors";
 
 createConnection().then(() => {
   const app = express();
-  const PORT = 4000;
+  const PORT = process.env.PORT || 4000;
 
   app.use(cors());
   app.use(express.json());
