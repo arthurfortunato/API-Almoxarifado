0e26e4fc86a6fd9ab9e69f05b62c238e16051914
arthurfortunato <luizarthur.fortunato@gmail.com>
2022-03-23 17:22:45 -0300

    ...

--- src/controller/CreateUserController.ts
+++ src/controller/CreateUserController.ts
@@ -3,29 +3,31 @@ import { CreateUserService } from "../services/CreateUserServices";
 
 export class CreateUserController {
   async signin(request: Request, response: Response) {
+    // @ts-ignore
     const { email, password } = request.body;
     const userService = new CreateUserService();
 
     const users = await userService.signin({ email, password });
-
+    // @ts-ignore
     return response.status(200).json(users);
   }
 
   async signup(request: Request, response: Response) {
     const user = request.body;
     const userService = new CreateUserService();
-
+    // @ts-ignore
     const users = await userService.signup(user);
-
+    // @ts-ignore
     return response.status(201).json(users);
   }
 
   async getUser(request: Request, response: Response) {
+    // @ts-ignore
     const user = request.user;
     const userService = new CreateUserService();
 
     const getUser = await userService.getUsers(user);
-
+    // @ts-ignore
     return response.status(200).json(getUser);
   }
 }
--- src/middleware/globalErrors.ts
+++ src/middleware/globalErrors.ts
@@ -1,8 +1,9 @@
 import { Request, Response, NextFunction } from 'express';
 import { AppError } from '../error/AppError';
-
+// @ts-ignore
 export const globalErrors = (err: Error, req: Request, res: Response, next: NextFunction) => {
     if (err instanceof AppError) {
+        // @ts-ignore
         res.status(err.statusCode).json({
             status: 'error',
             message: err.message,
@@ -11,7 +12,7 @@ export const globalErrors = (err: Error, req: Request, res: Response, next: Next
     }
 
     console.error(err);
-
+    // @ts-ignore
     return res.status(500).json({
         status: 'error',
         message: 'Internal server error',
