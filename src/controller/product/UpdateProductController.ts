1cb6c2eafa31bfc9ec408cf4fdfce0dd97180096
arthurfortunato <luizarthur.fortunato@gmail.com>
2022-03-27 14:51:12 -0300

    Ajustando as validações

--- src/controller/product/UpdateProductController.ts
+++ src/controller/product/UpdateProductController.ts
@@ -1,15 +1,33 @@
 import { Request, Response } from "express";
+import { AppError } from "../../error/AppError";
 import { UpdateProductService } from "../../services/product/UpdateProductService";
 
 export class UpdateProductController {
   async handle(request: Request, response: Response) {
+
     const { id } = request.params;
 
     const updateProductService = new UpdateProductService();
 
     const { name, code, sector, amount, description } = request.body;
 
-    await updateProductService.execute({
+    if (name === "" || !name) {
+      throw new AppError("Enter the product name", 404);
+    }
+    if (code === "" || !code) {
+      throw new AppError("Enter the product code", 404);
+    }
+    if (sector === "" || !sector) {
+      throw new AppError("Enter the product sector", 404);
+    }
+    if (amount === "" || !amount) {
+      throw new AppError("Enter the product amount", 404);
+    }
+    if (description === "" || !description) {
+      throw new AppError("Enter the product description", 404);
+    }
+
+    const updateProduct = await updateProductService.execute({
       id,
       name,
       code,
@@ -17,6 +35,6 @@ export class UpdateProductController {
       amount,
       description,
     });
-    return response.status(201).json();
+    return response.status(201).json(updateProduct);
   }
 }
